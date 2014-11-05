f4a220a8be7b9f7eef34b2800d53cb029a531e93
Joonas Pessi <joonas.pessi@futurice.com>
2014-11-05 11:44:12 +0200

    Pendulum body

--- js/game.js
+++ js/game.js
@@ -10,6 +10,7 @@ define(function (require) {
         this.world = new Box2D.b2World(new Box2D.b2Vec2(0.0, -10.0));
         this.initDegugDraw();
         this.initGround();
+        this.initBodies();
     }
 
     Game.prototype.initDegugDraw = function() {
@@ -24,36 +25,79 @@ define(function (require) {
     };
 
     Game.prototype.initGround = function() {
-
         var shape = new Box2D.b2EdgeShape();
-        shape.Set(new Box2D.b2Vec2(-40.0, 0.0), new Box2D.b2Vec2(40.0, 0.0));
+        shape.Set(new Box2D.b2Vec2(-100, 0), new Box2D.b2Vec2(100, 0));
 
         var ground = this.world.CreateBody(new Box2D.b2BodyDef());
         ground.CreateFixture(shape, 0.0);
+    };
 
-        var shape = new Box2D.b2PolygonShape();
-        shape.SetAsBox(0.25, 0.25);
-
-        var bd = new Box2D.b2BodyDef();
-        bd.set_type(Box2D.b2_dynamicBody);
-        bd.set_position(new Box2D.b2Vec2(-7.7, 15.0));
-        var b4 = this.world.CreateBody(bd);
-        b4.CreateFixture(shape, 10.0);
-
-
-
-
+    Game.prototype.initBodies = function() {
+        // Cart
         var shape2 = new Box2D.b2PolygonShape();
         shape2.SetAsBox(0.5,0.5);
 
+        var bd = new Box2D.b2BodyDef();
+        bd.set_type(Box2D.b2_dynamicBody);
+        bd.set_position(new Box2D.b2Vec2(0, 2));
+        bd.set_fixedRotation(true);
+
+        var cartBody = this.world.CreateBody(bd);
+        cartBody.CreateFixture(shape2, 2.0);
+
+        // Wheel
+        bd.set_fixedRotation(false);
+
+        var circleShape = new Box2D.b2CircleShape();
+        circleShape.set_m_radius(1);
+
+        var fd = new Box2D.b2FixtureDef();
+        fd.set_shape(circleShape);
+        fd.set_density(2);
+        fd.set_friction(1);
+        fd.get_filter().set_groupIndex(-1);
+
+        bd.set_position(new Box2D.b2Vec2(0, 2));
+        var wheelBody = this.world.CreateBody(bd);
+        wheelBody.CreateFixture(fd);
+
+        // Joint
+        var jd = new Box2D.b2WheelJointDef();
+        jd.set_bodyA(cartBody);
+        jd.set_bodyB(wheelBody);
+        jd.set_localAnchorA(new Box2D.b2Vec2(0, 0));
+        jd.set_localAnchorB(new Box2D.b2Vec2(0, 0));
+        jd.set_localAxisA(new Box2D.b2Vec2(0, 1));
+        jd.set_frequencyHz(20);
+        jd.set_dampingRatio(1);
+
+        var wheelJoint = Box2D.castObject(this.world.CreateJoint(jd), Box2D.b2WheelJoint);
+        wheelJoint.EnableMotor(true);
+        wheelJoint.SetMaxMotorTorque(500);
+        wheelJoint.SetMotorSpeed(1);
+
+        //Pendulum
+        var polygonShape = new Box2D.b2PolygonShape();
+        polygonShape.SetAsBox(0.5,5);
+
         var bd2 = new Box2D.b2BodyDef();
         bd2.set_type(Box2D.b2_dynamicBody);
-        bd2.set_position(new Box2D.b2Vec2(0, 2));
-        bd2.set_fixedRotation(true);
-        var cartBody = this.world.CreateBody(bd2);
+        bd2.set_position(new Box2D.b2Vec2(0, 2 + 5));
+        var pendulumBody = this.world.CreateBody(bd2);
 
-        cartBody.CreateFixture(shape2, 2.0);
-        debugger;
+        var fd2 = new Box2D.b2FixtureDef();
+        fd2.set_shape(polygonShape);
+        fd2.set_density(1);
+        fd2.get_filter().set_groupIndex(-1)
+        pendulumBody.CreateFixture(fd2);
+
+        var jd2 = new Box2D.b2RevoluteJointDef();
+        jd2.set_bodyA(cartBody);
+        jd2.set_bodyB(pendulumBody);
+        jd2.set_localAnchorA(new Box2D.b2Vec2(0.0,0.0));
+        jd2.set_localAnchorB(new Box2D.b2Vec2(0.0,-5.0));
+        jd2.set_collideConnected( false );
+        var pendulumJoint = Box2D.castObject(this.world.CreateJoint(jd2), Box2D.b2RevoluteJoint);
     };
 
     Game.prototype.update = function() {
